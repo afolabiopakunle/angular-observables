d851bd144d3c974010f8f0fb15c3a3632ad58b71
afolabiopakunle <opakunleafolabi@gmail.com>
2023-04-16 00:14:57 +0100

    project kick-started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
+import { UserComponent } from './user/user.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
+    AppRoutingModule,
   ],
   declarations: [
     AppComponent,
     HomeComponent,
-    UsersComponent,
+    UserComponent,
   ],
   bootstrap: [AppComponent]
 })
